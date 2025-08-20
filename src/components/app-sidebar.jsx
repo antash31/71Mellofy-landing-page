7600bd4f32fa7eea4e1215c78e4be8ee82bd7f69
Antash <antash.shrivastava@cars24.com>
2025-08-20 21:17:26 +0530

    fix: added appside bar

---
+++ src/components/app-sidebar.jsx
@@ -0,0 +1,37 @@
+"use client"
+
+import * as React from "react"
+import { NavMain } from "@/components/nav-main"
+import { NavProjects } from "@/components/nav-projects"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { ROUTES } from "@/constants/routes.constants"
+
+const data = ROUTES;
+
+export function AppSidebar({
+  ...props
+}) {
+  return (
+    (<Sidebar className="bg-accent" collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        {/* <NavMain items={data.navMain} /> */}
+        <NavProjects projects={data.projects} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>)
+  );
+}
